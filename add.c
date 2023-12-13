799f802c2f3b15e9e6ba46ef79a7255e8cc3e094
Jeraymi <99403069+Jeraymi@users.noreply.github.com>
2023-12-13 23:39:17 +0100

    Update add.c

--- add.c
+++ add.c
@@ -11,12 +11,12 @@ void f_add(stack_t **head, unsigned int counter)
 	int sum = 0, size = 0;
 
 	temp = *head;
-	while (temp) /* loop through the stack and count the nodes */
+	while (temp) 
 	{
 		temp = temp->next;
 		size++;
 	}
-	if (size < 2) /* check if the stack has at least two nodes */
+	if (size < 2) 
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
 		fclose(bus.file);
@@ -25,8 +25,8 @@ void f_add(stack_t **head, unsigned int counter)
 		exit(EXIT_FAILURE);
 	}
 	temp = *head;
-	sum = temp->n + temp->next->n; /* calculate the sum of the top two nodes */
-	temp->next->n = sum; /* assign the sum to the second node */
-	*head = temp->next; /* move the head pointer to the second node */
-	free(temp); /* free the first node */
+	sum = temp->n + temp->next->n; 
+	temp->next->n = sum; 
+	*head = temp->next; 
+	free(temp); 
 }
