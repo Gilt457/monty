cc5a394297a415c84fa1828d565c315ab57f54a9
Jeraymi <99403069+Jeraymi@users.noreply.github.com>
2023-12-13 23:51:20 +0100

    Update pint.c

--- pint.c
+++ pint.c
@@ -7,16 +7,13 @@
  */
 void f_pint(stack_t **head, unsigned int counter)
 {
-	/* check if the stack is empty */
 	if (*head == NULL)
 	{
-		/* print an error message and exit */
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	/* print the value at the top of the stack */
 	printf("%d\n", (*head)->n);
 }
